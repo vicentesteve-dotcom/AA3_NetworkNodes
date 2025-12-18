e20e769f3e11b6d72abbb997330c7e2ba7caa071
jordibarberan-maker <jordi.barberan@estudiant.enti.cat>
2025-12-18 16:34:16 +0100

    Nodo ja fet
    primera prova

--- src/Nodo.java
+++ src/Nodo.java
@@ -10,17 +10,48 @@ public class Nodo {
 
     Nodo(String nombre, String ip, boolean firewall, boolean vulnerable) {
         // TODO: Realizar constructor
+        vecinos = new ArrayList<>();
+        this.nombre = nombre;
+        this.ip = ip;
+        this.firewall = firewall;
+        this.vulnerable = vulnerable;
+        this.comprometido = false;
+
     }
 
         
     void conectar(Nodo otro) {
         // TODO: conecta este nodo con "otro" (sin duplicados)
+        if(!vecinos.contains(otro)) {
+            vecinos.add(otro)
+        }
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public boolean getVulnerable() {
+        return vulnerable;
+    }
+
+    public ArrayList<Nodo> getVecinos() {
+        return vecinos;
+    }
+
+    public void setComprometido(boolean c) {
+        this.comprometido = c;
     }
 
     @Override
     public String toString() {
         // TODO: devolver algo tipo:
         // "WebServer (172.16.0.10) [VULN] [PWN]" etc.
-        return "";
+        String exit = nombre;
+        exit += " (" + ip + ")";
+        exit += " [ Firewall: " + firewall + "]";
+        exit += " [ Vulnerable: " + vulnerable + "]";
+        exit += " [ Comprometido: " + comprometido + "]";
+        return exit;
     }
 }
